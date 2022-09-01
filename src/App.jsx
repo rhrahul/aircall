58500708ca1391a822829fe4106e56a804ba2d59
rhrahul <pipaliyarahulk@gmail.com>
2022-09-01 16:41:24 -0400

    Added TailwindCSS and Required Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container bg-slate-50">
+      <Header />
+      <div className="container-view bg-primary px-10 text-white">
+        Some activities should be here
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
